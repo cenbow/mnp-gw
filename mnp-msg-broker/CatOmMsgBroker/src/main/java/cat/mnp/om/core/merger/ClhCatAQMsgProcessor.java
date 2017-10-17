e552661b6dfa492e4f789ea09d11ba074f059073
bg <baggiony5@gmail.com>
2017-10-18 02:41:58 +0700

    PortInt AQ all

--- mnp-msg-broker/CatOmMsgBroker/src/main/java/cat/mnp/om/core/merger/ClhCatAQMsgProcessor.java
+++ mnp-msg-broker/CatOmMsgBroker/src/main/java/cat/mnp/om/core/merger/ClhCatAQMsgProcessor.java
@@ -2,9 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cat.mnp.mq.core;
+package cat.mnp.om.core.merger;
 
 import java.util.Enumeration;
+import java.util.Map;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -14,25 +15,23 @@ import javax.jms.TextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cat.mnp.mq.core.MsgHandler;
+
 /**
  *
  * @author HP-CAT
  */
-public class JmsMsgProcessor implements MessageListener {
+public class ClhCatAQMsgProcessor implements MessageListener {
 
-	private static final Logger logger = LoggerFactory.getLogger(JmsMsgProcessor.class);
-	private MsgHandler msgHandler;
-
-	public void setMsgHandler(MsgHandler msgHandler) {
-		this.msgHandler = msgHandler;
-	}
+	private static final Logger logger = LoggerFactory.getLogger(ClhCatAQMsgProcessor.class);
+	private Map<String, MsgHandler> msgHandlerMap;
 
 	@Override
 	public void onMessage(Message message) {
-    	try {
-			logger.info("[AQ] "+ message.getJMSDestination().toString() ); // TODO: miw debug config later
+		try {
+			logger.info("[AQ] " + message.getJMSDestination().toString()); // TODO: miw debug config later
 		} catch (JMSException e1) {
-			logger.error(e1.toString(),e1);
+			logger.error(e1.toString(), e1);
 		}
 		if (message instanceof TextMessage) {
 			try {
@@ -43,20 +42,27 @@ public class JmsMsgProcessor implements MessageListener {
 					Object obj = txtMsg.getObjectProperty(key);
 					if (obj != null) {
 						propStr += key + "=" + obj;
-						propStr += ",  ";
+						propStr += ", ";
 					}
 				}
 				logger.info(propStr);
-
+				String msgId = (String) txtMsg.getObjectProperty("MsgId");
+				MsgHandler msgHandler = msgHandlerMap.get(msgId);
+				logger.debug( msgHandler.getClass().getName());
 				msgHandler.processMsg(message); // direct, no mq
 
 			} catch (Exception ex) {
-				//throw new RuntimeException(ex); // retry transac
-				logger.error(ex.toString(),ex);
+				// throw new RuntimeException(ex); // retry transac
+				logger.error(ex.toString(), ex);
 
 			}
 		} else {
 			throw new IllegalArgumentException("Message must be of type TextMessage");
 		}
 	}
+
+	public void setMsgHandlerMap(Map<String, MsgHandler> msgHandlerMap) {
+		this.msgHandlerMap = msgHandlerMap;
+	}
+
 }
