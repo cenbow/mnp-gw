7aa62d5b65c7fe3efcd7a135106c0e6b60625256
bg <baggiony5@gmail.com>
2017-11-02 00:02:01 +0700

    update log + test

--- mnp-msg-broker/msg-broker-lib/src/main/java/cat/mnp/trace/TrackWorker.java
+++ mnp-msg-broker/msg-broker-lib/src/main/java/cat/mnp/trace/TrackWorker.java
@@ -1,10 +1,10 @@
-package com.javatpoint;
+package cat.mnp.trace;
 import org.aspectj.lang.JoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cat.mnp.mvno.dao.worker.Worker;
-public class TrackOperation {
+public class TrackWorker {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	public void myadvice(JoinPoint jp)  {// it is advice
 		log.debug(  jp.getTarget().toString()+" plSql: "+ ((Worker)jp.getTarget()).getPlSqlQuery());
