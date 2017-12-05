09a84eb226008db2e0f74c8c1836b5ab9c8c4c04
bg <baggiony5@gmail.com>
2017-12-05 19:34:32 +0700

    start stp

---
+++ mnp-msg-broker/ClhMsgBroker/src/main/java/cat/mnp/stp/core/StpBroadcastMgr.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.mnp.stp.core;
+
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.type.StandardBasicTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+
+import cat.mnp.mq.core.MsgHandlerBase;
+
+/**
+ *
+ * @author HP-CAT
+ */
+public class StpBroadcastMgr extends MsgHandlerBase {
+
+	private static final Logger logger = LoggerFactory.getLogger(StpBroadcastMgr.class);
+	private Map stpBroadcastMsgHandlerMap;
+	private SessionFactory sessionFactory;
+
+	@Override
+	public void processMsg(List<Message> msgList) throws Exception {
+		logger.info("select handler "+stpBroadcastMsgHandlerMap);
+		String sql = "select cat_mnp_inf_gw.check_order_type_donor ( '1122' ) orderType from dual ";
+		logger.info("checkOrderType: "+sql);
+		String type = (String) sessionFactory.getCurrentSession().createSQLQuery(sql).addScalar("orderType", StandardBasicTypes.STRING).uniqueResult();
+	}
+
+	public void setStpBroadcastMsgHandlerMap(Map stpBroadcastMsgHandlerMap) {
+		this.stpBroadcastMsgHandlerMap = stpBroadcastMsgHandlerMap;
+	}
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+}
