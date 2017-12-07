c77b8bca32a7ada5c60a565b1fa6334cdaab1eae
bg <baggiony5@gmail.com>
2017-12-08 00:19:25 +0700

    forgot last 1

--- mnp-ws/clh-ws-lib/src/main/java/com/telcordia/inpac/ws/package-info.java
+++ mnp-ws/clh-ws-lib/src/main/java/com/telcordia/inpac/ws/package-info.java
@@ -1,4 +1,5 @@
-@javax.xml.bind.annotation.XmlSchema(xmlns = {
-    @javax.xml.bind.annotation.XmlNs(prefix = "ns", namespaceURI = "http://ws.clearinghouse.com")
-}, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(
+		namespace = "http://ws.clearinghouse.com" // weblogic ns: workaround
+		, xmlns = {
+		@javax.xml.bind.annotation.XmlNs(prefix = "ns", namespaceURI = "http://ws.clearinghouse.com")}, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.telcordia.inpac.ws;
