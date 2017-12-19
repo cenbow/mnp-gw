de95f489ee777a91e0dbebe8f46a79a262dd3da3
bg <baggiony5@gmail.com>
2017-12-20 00:04:59 +0700

    PSResp

--- mnp-msg-broker/msg-broker-lib/src/main/java/cat/mnp/mvno/core/splitter/PortSyncRespMsgProcessor.java
+++ mnp-msg-broker/msg-broker-lib/src/main/java/cat/mnp/mvno/core/splitter/PortSyncRespMsgProcessor.java
@@ -4,10 +4,13 @@
  */
 package cat.mnp.mvno.core.splitter;
 
+import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.jms.JMSException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +26,8 @@ import com.telcordia.inpac.ws.jaxb.NPCMessageType;
 import cat.mnp.clh.util.NpcMessageUtils;
 import cat.mnp.mq.core.MsgHandlerBase;
 import cat.mnp.mvno.dao.MvnoMsgDao;
+import jaxb.clh.npcbulksync.ActivatedNumberType;
+import jaxb.clh.npcbulksync.NPCData;
 
 /**
  *
@@ -71,16 +76,27 @@ public class PortSyncRespMsgProcessor extends MsgHandlerBase {
 	}
 
 	public void processMsg(Message msg) throws Exception {
-		String msgString = new String (msg.getBody());
+		String msgString = new String(msg.getBody());
 		NPCMessageData npcMessageData = NpcMessageUtils.unMarshal(getJaxbUnMarshaller(), msgString);
 		NPCDataType npcDataType = npcMessageData.getNPCData();
 		MessageHeaderType messageHeader = npcDataType.getMessageHeader();
 		NPCMessageType npcMessages = npcDataType.getNPCMessages();
 
-
+		logger.debug(msgString);
 		// ftp file from clh?
+		String location = npcMessages.getSynchronisationResponse().get(0).getLocation();
+		logger.info(location);
 		// read file
+		File file = new File("C:\\Users\\baggio\\Desktop\\mnp-gw\\z-mnp-test/misc/portSyncRespFile.xml");
+		JAXBContext jaxbContext = JAXBContext.newInstance(NPCData.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		NPCData npcData = (NPCData) jaxbUnmarshaller.unmarshal(file);
+		logger.info("npcData: MessageName={}, IDNumber={}, NumberOfMessages={}", npcData.getMessageName(), npcData.getIDNumber(), npcData.getNumberOfMessages());
 		// write to DB
+		for (ActivatedNumberType a : npcData.getActivatedNumbers().getActivatedNumber()) {
+			logger.debug("Action={}, PortId={}, MSISDN={}, Donor={}, Recipient={}, ActivationDate={}", a.getAction(), a.getPortId(), a.getMSISDN(), a.getDonor(), a.getRecipient(), a.getActivationDate());
+		}
+		//MNP_PORT_SYNC_RESP , MNP_PORT_SYNC_RESP_NUMBER
 
 	}
 
---
+++ z-mnp-test/src/main/java/com/Test.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import jaxb.clh.npcbulksync.NPCData;
+
+public class Test {
+	public static void main(String[] args) {
+		try {
+			File file = new File("misc/portSyncRespFile.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(NPCData.class);
+
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			NPCData npcData = (NPCData) jaxbUnmarshaller.unmarshal(file);
+
+			System.out.println(npcData);
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+}
