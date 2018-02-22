11fccc15944b68431d12a8c3846379ce1322182d
bgm <baggiony5@gmail.com>
2018-02-22 21:47:54 +0700

    url ws test

--- z-mnp-test/src/main/java/miw/tc/GwTC.java
+++ z-mnp-test/src/main/java/miw/tc/GwTC.java
@@ -317,12 +317,12 @@ public class GwTC {
 		run_test(1000); // for consume 1010 Q
 	}
 
-	public void activateCatOmPortSync4001() throws Exception {
+	public void activateCatOmPortSync4001() throws Exception { // OM
 		logger.warn("activateCatOmPortSync: 4001)");
 		run_test(2000);
 	}
 
-	public void activateMvnoPortSync4001() throws Exception { // now use case
+	public void activateMvnoPortSync4001() throws Exception { // mvNo
 		logger.warn("activateMvnoPortSync: 4001)");
 		mvnoWs.send("MIW-rmv-4001.xml");
 	}
